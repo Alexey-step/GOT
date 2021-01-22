de1c28cb3e01d0cb49df46a841be544609360c8f
Aleksey <alexey-step@mail.ru>
2021-01-22 17:51:39 +0500

    GOTService

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,82 @@
-import React, {Component} from 'react';
 import './randomChar.css';
+import GotService from '../../services/gotService';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
+import React, {useState, useEffect} from 'react';
 
-export default class RandomChar extends Component {
+function RandomChar() {
 
-    render() {
+    const gotService = new GotService();
 
-        return (
-            <div className="random-block rounded">
-                <h4>Random Character: John</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>Anarchy</span>
-                    </li>
-                </ul>
-            </div>
-        );
+    const [char, setChar] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    
+    useEffect(() => {
+        updateChar();
+        const timerId = setInterval(updateChar, 15000);
+        return () => {
+            clearInterval(timerId);
+        }
+    }, [])
+
+    function onCharLoaded(char) {
+        setChar(char);
+        setLoading(false);
     }
+
+    function onError(err) {
+        setError(true);
+        setLoading(false);
+    }
+
+    function updateChar() {
+        const id = Math.floor(Math.random()*140 + 25);
+        gotService.getCharacter(id)
+            .then(onCharLoaded)
+            .catch(onError);
+    }
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View char={char}/> : null;
+
+    return (
+        <div className="random-block rounded">
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    );
 }
+
+
+
+const View = ({char}) => {
+    const {name, gender, born, died, culture} = char;
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Gender </span>
+                    <span>{gender}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Born </span>
+                    <span>{born}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Died </span>
+                    <span>{died}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Culture </span>
+                    <span>{culture}</span>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default RandomChar;
